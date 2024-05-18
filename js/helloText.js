15abdf31cd7d91bfd0e855d04f81cca77dd5f573
Junxe0 <nyun2i@naver.com>
2024-05-19 04:21:19 +0900

    Portfolio

--- js/helloText.js
+++ js/helloText.js
@@ -2,9 +2,8 @@ const HelloT = document.querySelectorAll(".textbox span");
 
 function TextEffect() {
   let timer = 2000;
-
   HelloT.forEach((item) => {
-    item.style.animation = `fade 500ms ${(timer += 100)}ms forwards`;
+    item.style.animation = `fadein 500ms ${(timer += 50)}ms forwards`;
   });
 }
 
--- js/menu.js
+++ js/menu.js
@@ -6,17 +6,19 @@ document.addEventListener('DOMContentLoaded', function() {
   HBGbttn.addEventListener('click', function() {
     if (!HBGbttn.classList.contains('active')) {
       HBGbttn.classList.add('active');
-      HBGMenu.style.display = "block"; // 요소를 보이게 설정
+      HBGMenu.style.visibility = "visible"; // visibility 속성을 사용하여 요소를 보이게 설정
+      HBGMenu.style.opacity = "1"; // opacity를 1로 설정하여 요소를 완전히 불투명하게 만듦
       setTimeout(() => {
         HBGMenu.style.width = "20%"; // 너비 변경
-      }, 10); // display 설정 후 약간의 지연을 주어 트랜지션 효과를 적용
+      }, 10); // visibility 및 opacity 설정 후 약간의 지연을 주어 트랜지션 효과를 적용
     } 
     else {
       HBGbttn.classList.remove('active');
       HBGMenu.style.width = "0"; // 너비를 0으로 설정
+      HBGMenu.style.opacity = "0"; // opacity를 0으로 설정하여 요소를 투명하게 만듦
       setTimeout(() => {
-        HBGMenu.style.display = "none"; // 트랜지션 후 요소를 숨김
-      }, 1000); // 트랜지션 시간(0.5초) 후에 display를 none으로 설정
+        HBGMenu.style.visibility = "hidden"; // 트랜지션 후 요소를 숨김
+      }, 1000); // 트랜지션 시간(1초) 후에 visibility를 hidden으로 설정
     }
   });
 });
