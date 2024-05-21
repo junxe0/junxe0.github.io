5fb52145a5fb8e242c27f51fa49dc5f21592131c
Junxe0 <nyun2i@naver.com>
2024-05-21 16:52:01 +0900

    Portfolio

--- js/href.js
+++ js/href.js
@@ -3,7 +3,9 @@ const DBDiscordPy = document.querySelector("#DBDiscordPy"); // Discord.py 2.0
 const BSHGithub = document.querySelector("#BSHGithub"); // 부산헹 깃허브
 const PTFLOGithub = document.querySelector("#PTFLOGithub"); // 포트폴리오 깃허브
 const DWCGithub = document.querySelector("#DWCGithub"); // 디코 웹사이트 카피 깃허브
-const GDGGithub =  document.querySelector("#GDGGithub"); // 크롬 공룡 게임 카피 깃허브
+const DWCWebSite = document.querySelector("#DWCWebSite"); // 디코 웹사이트 카피 웹사이트
+const GDGGithub = document.querySelector("#GDGGithub"); // 크롬 공룡 게임 카피 깃허브
+const GDGGameCopySite = document.querySelector("#GDGGameCopySite"); // 크롬 공룡 게임 카피 사이트
 
 function gotopage(link) {
   setTimeout(() => {
@@ -34,6 +36,16 @@ DWCGithub.addEventListener("click", () => {
   showAlert("현재 비활성화 되어있는 메뉴입니다!");
 })
 
+DWCWebSite.addEventListener("click", () => {
+  showAlert("3초 후 창이 열립니다.");
+  gotopage("https://discord.com/");
+})
+
 GDGGithub.addEventListener("click", () => {
   showAlert("현재 비활성화 되어있는 메뉴입니다!");
+})
+
+GDGGameCopySite.addEventListener("click", () => {
+  showAlert("3초 후 창이 열립니다.");
+  gotopage("https://dinorunner.com/ko/");
 })
